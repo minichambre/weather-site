28fca75bc593f970a5df2c8812bd53f90c7880c3
Daniel Gowing <minichambre95@gmail.com>
2019-04-22 23:37:58 +0300

    added templating and api request to display basic results

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const axios = require("axios");
 const app = express()
 const port = 81
+const {apiKey} = require('./config.json');
 var mustacheExpress = require('mustache-express');
 var path = require('path');
+const url = "https://api.weatherbit.io/v2.0/current?city=rovaniemi,fi&key=" + apiKey;
+const https = require("https");
 
 app.engine('html', mustacheExpress())
 // app.use(express.static(path.join(__dirname, '/public')));
@@ -10,7 +14,28 @@ app.use(express.static('public'))
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.get('/', function(req, res) {
-    res.render("hello.html", {"yourname": "World"});
+    getWeather(res);
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+function getWeather(response){
+  https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    console.log(data);
+    body += data;
+  });
+  res.on("end", () => {
+    var weather = JSON.parse(body)['data'][0];
+    response.render("hello.html", {
+      "description": weather['weather']['description'],
+      "temp":weather['temp']
+    });
+  });
+});
+}
