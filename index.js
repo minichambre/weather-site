8bc318de62fc6f5e94ceed9db5c6f966e119fbbc
Daniel Gowing <minichambre95@gmail.com>
2019-04-23 18:08:42 +0300

    updated styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require("axios");
 const app = express()
-const port = process.env.PORT || 81
+const port = process.env.PORT || 8080
 //const {apiKey} = require('./config.json'); //make .env
 const apiKey = process.env.APIKEY
 var mustacheExpress = require('mustache-express');
