3aa2a473d6782e62f2a405a770f60aed16816250
Daniel Gowing <minichambre95@gmail.com>
2019-04-24 09:20:02 +0300

    added ajax request to change city to inspect

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
-const express = require('express')
+const express = require("express");
 const axios = require("axios");
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 //const {apiKey} = require('./config.json'); //make .env
-const apiKey = process.env.APIKEY
-var mustacheExpress = require('mustache-express');
-var path = require('path');
-const url = "https://api.weatherbit.io/v2.0/current?city=rovaniemi,fi&key=" + apiKey;
+const apiKey = process.env.APIKEY;
+var mustacheExpress = require("mustache-express");
+var path = require("path");
+
 const https = require("https");
 
-app.engine('html', mustacheExpress())
+app.engine("html", mustacheExpress());
 // app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static('public'))
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.get('/', function(req, res) {
-    getWeather(res);
-
+app.use(express.static("public"));
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+app.get("/", function(req, res) {
+  getWeather(res, "rovaniemi,fi");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/api/get", function(req, res) {
 
+  let city = req.query.city;
+  getWeatherAjax(res,city);
+});
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-function getWeather(response){
+function getWeather(response, city) {
+  const url =
+    "https://api.weatherbit.io/v2.0/current?city="+ city + "&key=" + apiKey;
+
   https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    console.log(data);
-    body += data;
-  });
-  res.on("end", () => {
-    var weather = JSON.parse(body)['data'][0];
-    response.render("hello.html", {
-      "description": weather['weather']['description'],
-      "temp":weather['temp']
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      console.log(data);
+      body += data;
+    });
+    res.on("end", () => {
+      var weather = JSON.parse(body)["data"][0];
+      response.render("main.html", {
+        description: weather["weather"]["description"],
+        temp: weather["temp"],
+        location: weather["city_name"],
+        country_code: weather["country_code"]
+      });
     });
   });
-});
+}
+
+function getWeatherAjax(response, city) {
+  const url =
+    "https://api.weatherbit.io/v2.0/current?city="+ city + "&key=" + apiKey;
+
+  https.get(url, res => {
+    res.setEncoding("utf8");
+    let body = "";
+    res.on("data", data => {
+      console.log(data);
+      body += data;
+    });
+    res.on("end", () => {
+      if (body){
+        var weather = {"status": 1, "data": JSON.parse(body)["data"][0]};
+      } else {
+        var weather = {"status": 2}
+      }
+
+      response.send(weather);
+    });
+  });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
-const {apiKey} = require("config.json");
-
-function testScript(){
-  document.getElementById('test').innerHTML = apiKey;
+function submitCity() {
+  let city = document.getElementById("cityInput");
+  let temp = document.getElementById("temp");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+  fetch("/api/get?city=" + city.value)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      if (myJson.status === 1) {
+        var weather = myJson.data;
+      } else {
+        console.log("Error occurred");
+        return;
+      }
+      temp.innerHTML = weather["temp"] + " degrees celcius";
+      description.innerHTML = weather["weather"]["description"];
+      location.innerHTML = "in " + weather["city_name"] +", " + weather["country_code"];
+    });
 }
