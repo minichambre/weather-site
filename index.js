cc68bf9498a2d83ade2b211c82a8166c04a5818e
minichambre <minichambre95@gmail.com>
2019-06-21 21:51:01 +0100

    added dotenv

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 8080;
-//const {apiKey} = require('./config.json'); //make .env
 const apiKey = process.env.APIKEY;
 var mustacheExpress = require("mustache-express");
 var path = require("path");
